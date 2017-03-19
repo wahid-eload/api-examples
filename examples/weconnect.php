5a5398856a274cc5d9666cc0d6de9b40e490dd3e
Wahid eLoad <Wahid.eLoad@gmail.com>
2017-03-19 02:58:44 +0100

    remove the detail flag

--- examples/weconnect.php
+++ examples/weconnect.php
@@ -2,5 +2,5 @@
   require_once("init.php");
   $we = new WeConnect();
   $we->debug(True); //This is just for testing, please do not add such a line in production mode.
-  $xml = $we->request("25", array('number'=>3122244506),1);
+  $xml = $we->request("25", array('number'=>3122244506));
 ?>
